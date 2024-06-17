7f9b00a276fccbb9cc356adde2530b75a46d4eda
Kyle <kagilstrawork@gmail.com>
2024-06-17 15:14:15 -0700

    Fallout1 tag display, bound values, etc

--- FallloutCharacterCreator/Fallout1/Fallout1CharCreation.xaml.cs
+++ FallloutCharacterCreator/Fallout1/Fallout1CharCreation.xaml.cs
@@ -21,9 +21,56 @@ namespace FallloutCharacterCreator.Fallout1
     /// </summary>
     public partial class Fallout1CharCreation : Page//, INotifyPropertyChanged
     {
-        public string TaggedSkillsText => $"{Character.TaggedSkills}/3";
+        
         public Fallout1Character Character { get; set; }
         public List<Fallout1Character> characterSnapshots = new List<Fallout1Character>();
+        
+        //Bound Strings for SPECIAL Values
+        public string StrengthText => $"{Character.Strength}";
+        public string PerceptionText => $"{Character.Perception}";
+        public string EnduranceText => $"{Character.Endurance}";
+        public string CharismaText => $"{Character.Charisma}";
+        public string IntelligenceText => $"{Character.Intelligence}";
+        public string AgilityText => $"{Character.Agility}";
+        public string LuckText => $"{Character.Luck}";
+        public string SpecialPointsText => $"{Character.SpecialPoints}";
+
+        //Bound Strings for Skill values
+        public string SmallGunsText => $"{Character.SmallGuns}%";
+        public string BigGunsText => $"{Character.BigGuns}%";
+        public string EnergyWeaponsText => $"{Character.EnergyWeapons}%";
+        public string UnarmedText => $"{Character.Unarmed}%";
+        public string MeleeWeaponsText => $"{Character.MeleeWeapons}%";
+        public string ThrowingText => $"{Character.Throwing}%";
+        public string FirstAidText => $"{Character.FirstAid}%";
+        public string DoctorText => $"{Character.Doctor}%";
+        public string SneakText => $"{Character.Sneak}%";
+        public string LockpickText => $"{Character.Lockpick}%";
+        public string StealText => $"{Character.Steal}%";
+        public string TrapsText => $"{Character.Traps}%";
+        public string ScienceText => $"{Character.Science}%";
+        public string RepairText => $"{Character.Repair}%";
+        public string SpeechText => $"{Character.Speech}%";
+        public string BarterText => $"{Character.Barter}%";
+        public string GamblingText => $"{Character.Gambling}%";
+        public string OutdoorsmanText => $"{Character.Outdoorsman}%";
+        public string TaggedSkillsText => $"{Character.TaggedSkills}/3";
+
+
+        //HitPoints = this.HitPoints,
+        //Level = this.Level,
+        //ArmorClass = this.ArmorClass,
+        //ActionPoints = this.ActionPoints,
+        //CarryWeight = this.CarryWeight,
+        //MeleeDamage = this.MeleeDamage,
+        //DamageRes = this.DamageRes,
+        //PoisonRes = this.PoisonRes,
+        //RadiationRes = this.RadiationRes,
+        //Sequence = this.Sequence,
+        //HealingRate = this.HealingRate,
+        //CriticalChance = this.CriticalChance,
+        //SkillPoints = this.SkillPoints,
+
         public Fallout1CharCreation()
         {
             InitializeComponent();
